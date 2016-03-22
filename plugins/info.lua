d66dafac195734147dc2779bd45312e963db9039
dddaaaddd <r.zasdf082@gmail.com>
2016-03-22 03:22:31 -0700

    Update info.lua

--- plugins/info.lua
+++ plugins/info.lua
@@ -84,7 +84,7 @@ local function action_by_id(extra, success, result)  -- /info <ID> function
   local um_hash = 'msgs:'..result.id..':'..extra.chat2
   user_info_msgs = tonumber(redis:get(um_hash) or 0)
   text = text..'Total messages : '..user_info_msgs..'\n\n'
-  text = text..'#Sbss_Team'
+  text = text..'#MAKS_Team'
   send_msg(extra.receiver, text, ok_cb,  true)
   else
   send_msg(extra.receiver, 'id not found.\nuse : /info @username', ok_cb, false)
@@ -123,7 +123,7 @@ local function action_by_reply(extra, success, result)-- (reply) /info  function
   local um_hash = 'msgs:'..result.from.id..':'..result.to.id
   user_info_msgs = tonumber(redis:get(um_hash) or 0)
   text = text..'Total messages : '..user_info_msgs..'\n\n'
-  text = text..'#Sbss_Team'
+  text = text..'#MAKS_Team'
   send_msg(extra.receiver, text, ok_cb, true)
 end
 
