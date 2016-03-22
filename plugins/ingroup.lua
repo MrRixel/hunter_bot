6fe69dabf7c7c72af2a93590f86d8e2a3072e92b
dddaaaddd <r.zasdf082@gmail.com>
2016-03-22 05:38:16 -0700

    Update ingroup.lua

--- plugins/ingroup.lua
+++ plugins/ingroup.lua
@@ -196,7 +196,7 @@ local function show_group_settingsmod(msg, data, target)
                 NUM_MSG_MAX = 0
         end
     end
-    local tmp = '\ngithub.com/telemanagerplus\n\nchannel :@telemanager_ch'
+    local tmp = '/MAX_TM\n\nchannel :@MAX_TM'
     local bots_protection = "Yes"
     if data[tostring(msg.to.id)]['settings']['lock_bots'] then
         bots_protection = data[tostring(msg.to.id)]['settings']['lock_bots']
