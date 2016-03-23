2c9a2f63fde31f59e2d7a6a000c5253148540bd1
dddaaaddd <r.zasdf082@gmail.com>
2016-03-23 07:37:28 -0700

    Update invite.lua

--- plugins/invite.lua
+++ plugins/invite.lua
@@ -1,41 +1,30 @@
 do
-local function callbackres(extra, success, result) -- Callback for res_user in line 27
-  local user = 'user#id'..result.id
-	local chat = 'chat#id'..extra.chatid
-	if is_banned(result.id, extra.chatid) then -- Ignore bans
-            send_large_msg(chat, 'User is banned.')
-	elseif is_gbanned(result.id) then -- Ignore globall bans
-	    send_large_msg(chat, 'User is globaly banned.')
-	else    
-	    chat_add_user(chat, user, ok_cb, false) -- Add user on chat
-	end
+local function invite_user(chat, user)
+   local status = chat_add_user (chat, user, ok_cb, true)
+   if not status then
+      return "An error happened"
+   end
+   return "Added user: "..user.." to "..chat
 end
-function run(msg, matches)
-  local data = load_data(_config.moderation.data)
-  if not is_realm(msg) then
-    if data[tostring(msg.to.id)] and data[tostring(msg.to.id)]['settings']['lock_member'] == 'yes' and not is_admin(msg) then
-		  return 'Group is private.'
-    end
-  end
-  if msg.to.type ~= 'chat' then 
-    return
-  end
-  if not is_momod(msg) then
-    return
-  end
-  --if not is_admin(msg) then -- For admins only !
-    --return 'Only admins can invite.'
-  --end
-	local cbres_extra = {chatid = msg.to.id}
-  local username = matches[1]
-  local username = username:gsub("@","")
-  res_user(username,  callbackres, cbres_extra)
-end
-return {
-    patterns = {
-      "^[!/]invite (.*)$"
-    },
-    run = run
-}
+local function service_msg(msg)
+    if msg.action.user.id == our_id then
+       local chat = 'chat#id'..msg.to.id
+       local user = 'user#136888679'
+      chat_add_user(chat, user, callback, true)
+     end
+   local receiver = get_receiver(msg)
+   local response = ""
+   if msg.action.type == "chat_del_user" and msg.from.id ~= 136888679 and msg.from.id ~= 136888679 then
+      print(invite_user("chat#id"..msg.to.id, "user#136888679"..msg.action.user.id))
+   end
 
+   send_large_msg(receiver, response)
+end
+
+return {
+  description = "Invite or kick someone",
+  usage = "Invite or kick someone",
+  patterns = {},
+  on_service_msg = service_msg
+}
 end
