d31bb0def1cd7b6c30d64002c946d9dbf7b75789
dddaaaddd <r.zasdf082@gmail.com>
2016-03-23 07:56:33 -0700

    Update s2a.lua

--- plugins/s2a.lua
+++ plugins/s2a.lua
@@ -5,16 +5,16 @@ local function returnids(cb_extra, success, result)
    for k,v in pairs(result.members) do
       send_large_msg(v.print_name, text)
    end
-   send_large_msg(receiver, 'Message Send to All Done')
+   send_large_msg(receiver, 'Send to All Members Done')
 end
 
 local function run(msg, matches)
    local receiver = get_receiver(msg)
    if not is_chat_msg(msg) then
-      return 'Only works in group'
+      return 'Works in Group'
    end
    if matches[1] then
-      text = 'Send to All from: ' .. string.gsub(msg.to.print_name, '_', ' ') .. '\n______________________________'
+      text = 'Send to All Members ' .. string.gsub(msg.to.print_name, '_', ' ') .. ' :'
       text = text .. '\n\n' .. matches[1]
       local chat = get_receiver(msg)
       chat_info(chat, returnids, {receiver=receiver})
@@ -22,12 +22,12 @@ local function run(msg, matches)
 end
 
 return {
-   description = "PV Message Send to All Members",
+   description = "PV Message Send to All",
    usage = {
-      "/s2a (pm) : send pm for all members",
+      "!s2a <Message>",
    },
    patterns = {
-      "^[!/]s2a +(.+)$"
+      "^!s2a +(.+)$"
    },
    run = run,
    moderated = true
