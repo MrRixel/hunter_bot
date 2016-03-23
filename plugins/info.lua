9e5220bd8d6630014827034a0bdfe0cf1a59cb1e
dddaaaddd <r.zasdf082@gmail.com>
2016-03-23 02:51:48 -0700

    Update info.lua

--- plugins/info.lua
+++ plugins/info.lua
@@ -203,7 +203,7 @@ local function run(msg, matches)
 	 text = text..'Group name : '..msg.to.title..'\n'
      text = text..'Group ID : '..msg.to.id
     end
-	text = text..'\n\n#MAX_Team'
+	text = text..'\n\n#MAX_TEAM'
     return send_msg(receiver, text, ok_cb, true)
     end
   end
